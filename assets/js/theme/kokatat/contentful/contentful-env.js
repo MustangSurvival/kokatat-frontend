78c2f7648edaea95cc7a3b09e567ca328d0e28e6
cmrotzek-wing <cmrotzek@inflatablesolutions.com>
2026-06-30 10:10:35 -0700

    fix: contentful

--- assets/js/theme/kokatat/contentful/contentful-env.js
+++ assets/js/theme/kokatat/contentful/contentful-env.js
@@ -3,7 +3,7 @@ export default class ContentfulEnvironment {
     constructor() {
         this.space = 'pe7xw34fs2z8';
         this.environment = 'master';
-        this.access_token = 'h4PaUm2LJskfNE-aiBNCAU2FI0eHDHylgC6nf0ABeuc';
+        this.access_token = 'fRLgPhYKXctwEt0KnjaHZdDBlsG8teCerIAkeooMOFw';
     }
 }
 
